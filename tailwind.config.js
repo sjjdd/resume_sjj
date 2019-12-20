dbd386f0615404c66c71fd02c5e8e9b3bd191b42
sjjdd <1667688587@qq.com>
2019-12-20 22:04:39 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  theme: {
+    extend: {
+        spacing:{
+            '72':'18rem',
+            '80':'20rem',
+            '88':'22rem',
+            '96':'24rem',
+            '104':'26rem',
+            '112':'28rem',
+            '120':'30rem',
+            '128':'32rem',
+            '130':'34rem',
+            '132':'36rem',
+            '134':'38rem',
+            '136':'40rem',
+            '138':'42rem',
+            '140':'44rem',
+            '142':'46rem',
+            '144':'48rem',
+            '146':'50rem',
+            '148':'52rem',
+            '150':'54rem'
+        },
+    fontSize:{
+            'll':'.468rem',
+    },
+    }
+  },
+  variants: {
+      backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: []
+}
